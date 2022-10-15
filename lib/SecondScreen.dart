a4ad45f6bd07302e9baf815ccb16c4025f477591
rizwan pochi <rizwanpochi11@gmail.com>
2022-10-15 23:50:12 +0530

    Added new Screen

--- lib/SecondScreen.dart
+++ lib/SecondScreen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
-class SecondScreen extends StatelessWidget{
+
+class SecondScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Linkedln"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'SEARCH',
+            style: TextStyle(
+                fontSize: 40, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'POSTS',
+            style: TextStyle(
+                fontSize: 40, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'JOBS',
+            style: TextStyle(
+                fontSize: 40, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:front/SecondScreen.dart';
+import 'SecondScreen.dart';
 
-
-
-import 'SecondScreen.dart';void main() {
+void main() {
   runApp(const MyApp());
 }
 
@@ -18,7 +17,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+        home: new HomeScreen());
+  }
+}
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+ return new Scaffold(
         appBar: AppBar(
           title: Text('login'),
         ),
@@ -26,80 +31,80 @@ class _MyAppState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              'login',
-              style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.teal,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Form(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal : 20),
-                      child: TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            hintText: 'Enter email',
-                            prefixIcon: Icon(Icons.email),
-                            border: OutlineInputBorder(),
-                          ),
-                          onChanged: (String value) {
+          Text(
+          'login',
+          style: TextStyle(
+              fontSize: 40,
+              color: Colors.teal,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Form(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {
 
-                          },
-                          validator: (value) {
-                            return value!.isEmpty ? 'please enter email' : null;
-                          }
-                      ),
-                    ),
-                    SizedBox(height: 30,),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextFormField(
-                          keyboardType: TextInputType.visiblePassword,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            hintText: 'Enter Password',
-                            prefixIcon: Icon(Icons.password),
-                            border: OutlineInputBorder(),
-                          ),
-                          onChanged: (String value) {
-
-                          },
-                          validator: (value) {
-                            return value!.isEmpty ? 'please enter email' : null;
-                          }
-                      ),
-                    ),
-                    SizedBox(height: 30,),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 50),
-                      child: MaterialButton(
-                          onPressed: (){
-                            Navigator.push(context, new MaterialPageRoute(
-                                builder: (context) => SecondScreen()
-                            ));
-                          },
-                        minWidth: double.infinity,
-                        child: Text('Login'),
-                        color: Colors.blue,
-                        textColor: Colors.white,
-                      ),
-                    ),
-                  ],
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'please enter email' : null;
+                      }
+                  ),
                 ),
-              ),
+                SizedBox(height: 30,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter Password',
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {
+
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'please enter email' : null;
+                      }
+                  ),
+                ),
+                SizedBox(height: 30,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.push(context, new MaterialPageRoute(
+                          builder: (context) => SecondScreen()
+                      ));
+                    },
+                    minWidth: double.infinity,
+                    child: Text('Login'),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                  ),
+                ),
+              ],
             ),
+          ),
+        ),
           ],
         ),
-      ),
-    );
+      );
   }
 }
+
 
 
