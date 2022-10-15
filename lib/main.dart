b1f1b66d5342df1183b55ef6f1b492e77c54b03b
rizwan pochi <rizwanpochi11@gmail.com>
2022-10-15 19:41:57 +0530

    login page

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:front/SecondScreen.dart';
+
+
+
+import 'SecondScreen.dart';void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('login'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'login',
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Form(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal : 20),
+                      child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            hintText: 'Enter email',
+                            prefixIcon: Icon(Icons.email),
+                            border: OutlineInputBorder(),
+                          ),
+                          onChanged: (String value) {
+
+                          },
+                          validator: (value) {
+                            return value!.isEmpty ? 'please enter email' : null;
+                          }
+                      ),
+                    ),
+                    SizedBox(height: 30,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            hintText: 'Enter Password',
+                            prefixIcon: Icon(Icons.password),
+                            border: OutlineInputBorder(),
+                          ),
+                          onChanged: (String value) {
+
+                          },
+                          validator: (value) {
+                            return value!.isEmpty ? 'please enter email' : null;
+                          }
+                      ),
+                    ),
+                    SizedBox(height: 30,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      child: MaterialButton(
+                          onPressed: (){
+                            Navigator.push(context, new MaterialPageRoute(
+                                builder: (context) => SecondScreen()
+                            ));
+                          },
+                        minWidth: double.infinity,
+                        child: Text('Login'),
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
